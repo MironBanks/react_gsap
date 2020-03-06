a3329d836027ddc2ef54e948542c50d698f79d56
Miron Banks <madapegarage@gmail.com>
2020-03-06 23:45:31 +0100

    gsap with react

---
+++ src/pages/about.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Title from "../components/title"
+
+const About = () => {
+    return (
+        <div className="inner">
+            <Title lineContent="This is the" lineContent2="about page" />
+            <div>
+                <p className="info">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nec leo a augue porttitor pellentesque
+                    non et est. Duis placerat ultrices lobortis. Orci varius natoque penatibus et magnis dis parturient montes,
+                    nascetur ridiculus mus. Vestibulum lacus ligula, laoreet quis gravida at, blandit id metus.
+                    Pellentesque sit amet ligula quis felis pulvinar varius. In pretium urna eget varius gravida.
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default About;
