cab70dc0dbc27c916c02888567d48757bd0ba794
Miron Banks <madapegarage@gmail.com>
2020-03-07 21:23:59 +0100

    pic

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react'
+import { gsap } from "gsap"
 import Title from '../components/title'
 
 const Home = () => {
+
+    let line3 = useRef(null)
+
+    useEffect(() => {
+        gsap.from([line3], 0.8,
+            {
+                y: 30,
+                delay: 1.5,
+                ease: "power3.InOut",
+                opacity: 0,
+                stagger: {
+                    amount: 0.6
+                }
+            })
+
+    }, [])
+
     return (
-        <div className="inner">
-            <Title lineContent="miron banks" lineContent2="i build things for the web." />
-            <div>
-                <p className="info">
-                    Hello! I'm Miron, a Front-End developer based in Oslo who enjoys building things that
-                    live on the internet. I develop exceptional websites and web apps that provide intuitive,
-                    pixel-perfect user interfaces.
+        <>
+            <div className="inner">
+                <Title lineContent="miron banks" lineContent2="i build things for the web." />
+                <div>
+                    <p className="info">
+                        Hello! I'm Miron, a Front-End developer based in Oslo who enjoys building things that
+                        live on the internet. I develop exceptional websites and web apps that provide intuitive,
+                        pixel-perfect user interfaces.
                 </p>
+                </div>
             </div>
-        </div>
+            <div ref={el => line3 = el} className="line">
+                <img />
+            </div>
+        </>
     );
 }
 
